711afd4693b71ce0029724652f2b0fccd6bff59c
Aakarsh Arora <aroraaakarsh0@gmail.com>
2024-08-29 17:47:42 +0530

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,12 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+            <li class=" nav-item"><a href="index.html"><i class="la la-home"></i><span class="menu-title" data-i18n="Dashboard">Dashboard</span></a>
                 <ul class="menu-content">
                     @if (auth()->user()->role_id==1)
                     <li class="active"><a class="menu-item" href="/createjob"><i></i><span data-i18n="eCommerce">Create Job</span></a>
                     </li>
                     <li><a class="menu-item" href="/mycreatedjobs"><i></i><span data-i18n="Crypto">View Jobs</span></a>
-                    </li>
-                    <li><a class="menu-item" href="dashboard-sales.html"><i></i><span data-i18n="Sales">Extra</span></a>
                     </li>
                     @elseif(auth()->user()->role_id==2)
                     <li class="active"><a class="menu-item" href="/alljobs"><i></i><span data-i18n="eCommerce">Search Jobs</span></a>
